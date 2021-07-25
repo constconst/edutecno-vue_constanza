c6ddaede6d93249eb1eab3077e4305267a2416d5
Constanza Barrios <co.fuenzalida@gmail.com>
2021-07-24 20:00:33 -0400

    añadí Desafío Pokeguia (los pasos anteriores se pueden ver en el antiguo repositorio

---
+++ Desafio_Pokeguia/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <img alt="logo" src="./assets/
+">
+    <PokeGuia />
+  </div>
+</template>
+
+<script>
+import PokeGuia from 'components/PokeGuia.vue'
+export default {
+  name: 'App',
+  components: {
+    PokeGuia
+  }
+}
+</script>
---
+++ Desafio_Pokeguia/components/pokeGuia.vue
@@ -0,0 +1,22 @@
+<script>
+// Fetch es la forma más simple de hacer llamadas a una api.
+export default {
+    data(){
+        return {
+            img:'dsdlkslsk',
+            nombre: '',
+            habilidades:'',
+            movimientos:'',            
+        };
+    },
+    computed: {
+        infoPokemon(){
+            return this.img + this.nombre + this.habilidades + this.movimientos
+    },
+    created() {
+        fetch( "https://pokeapi.co/api/v2/pokemon/nombre_pokemon" )
+        .then(response => response.json())
+        .then(json => console .log(json));      
+    }
+}
+</script>
